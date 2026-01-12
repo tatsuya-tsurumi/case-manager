a1ba5112b9b5d0ebee0db9658fb988a29bd7dffc
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2026-01-12 17:48:31 +0900

    com.exampleの階層構造部分修正

--- src/main/java/com/example/casemanager/CaseManagerApplication.java
+++ src/main/java/com/example/casemanager/CaseManagerApplication.java
@@ -1,4 +1,4 @@
-package com.example.muser;
+package com.example.casemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
