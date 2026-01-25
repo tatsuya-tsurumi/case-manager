f12fe1ff2318c28a09c45812a5d7181f81f3ee14
Tsurumi Tatsuya <93573854+tatsuya-tsurumi@users.noreply.github.com>
2026-01-25 16:46:49 +0900

    バリデーションの内容を正しい内容へ修正。

--- src/main/java/com/example/casemanager/form/CaseEditForm.java
+++ src/main/java/com/example/casemanager/form/CaseEditForm.java
@@ -1,19 +1,22 @@
 package com.example.casemanager.form;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CaseEditForm {
 	private Integer caseId;
+	
+	@NotNull(message = "ユーザを選択してください")
 	private String userId;
 	private String userName;
 	@Size(min=1, max=32, message="1文字から32文字で指定してください")
 	private String caseName;
-	@Size(min=1, max=32, message="1文字から32文字で指定してください")
+	@Size(max=32, message="32文字以内で指定してください")
 	private String clientName;
 	private String detail;
-	@Size(min=2, max=2, message="指定に誤りがあります")
+	@NotNull(message = "ステータスを選択してください")
 	private String statusCode;
 	private String statusName;
 }
--- src/main/java/com/example/casemanager/form/TaskRegistForm.java
+++ src/main/java/com/example/casemanager/form/TaskRegistForm.java
@@ -2,6 +2,7 @@ package com.example.casemanager.form;
 
 import java.sql.Date;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,14 +10,16 @@ import lombok.Data;
 public class TaskRegistForm {
 	private Integer caseId;
 	
-	@Size(min=1, max=200, message="1文字から200文字で指定してください")
+	@Size(min=1, max=64, message="1文字から64文字で指定してください")
 	private String title;
 	
 	private Date deadline;
 	
+	@NotNull(message = "重要度を選択してください")
 	private String priorityCode;
 	private String priorityName;
 	
+	@NotNull(message = "ステータスを選択してください")
 	private String statusCode;
 	private String statusName;
 }
